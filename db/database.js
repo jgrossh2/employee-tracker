20ceba712cb8388d1ac1105f5e6ee4f8da38d2f2
Mona Lisa <joannagrosshans2@gmail.com>
2020-11-18 13:17:41 -0600

    database connected

--- db/database.js
+++ db/database.js
@@ -1,17 +1,10 @@
 const mysql = require('mysql2');
 
 //connect to database
-// const db = new mysql2.Database('./db/employee.db', err => {
-//     if (err) {
-//         return console.error(err.message);
-//     }
-
-//     console.log('Connected to the employee database.');
-// });
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'test'
+    database: 'employee'
 });
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const table = require('console.table');
 const inquire = require('inquirer');
+const express = require('express');
 const db = require('./db/database');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//Express middleware
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+// //Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //use apiRoutes
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
 
-//Default response for any other requests(Not found) Catch all
+// //Default response for any other requests(Not found) Catch all
 app.use((req, res) => {
     res.status(404).end();
 });
