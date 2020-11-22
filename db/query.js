27dd81cfd8d6bda99648405b0db783b53780cbdb
Mona Lisa <joannagrosshans2@gmail.com>
2020-11-22 15:59:59 -0600

    choices all function

--- db/query.js
+++ db/query.js
@@ -18,7 +18,7 @@ class DB {
     viewEmployees() {
         return this.connection.promise().query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department ON role.department_id=department.id LEFT JOIN employee AS manager ON manager.id=employee.manager_id');
     };
-    viewEmployeesDepartment(choice){
+    viewEmployeesDepartment(choice) {
         return this.connection.promise().query('SELECT employee.id AS employee_id, CONCAT(first_name, " ", last_name) AS name FROM employee LEFT JOIN role ON role_id= role.id LEFT JOIN department ON department_id=department.id WHERE department.id = ?', choice);
     }
     //query add department==asked to add name, then added to database
